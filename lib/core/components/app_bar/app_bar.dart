b64b1153c82e20e15ddb1c0946c29a8e3e932f49
Fudy-V <fudyV.test@gmail.com>
2024-11-07 01:36:36 +0900

    ✨ Tabbarの追加

--- lib/core/components/app_bar/app_bar.dart
+++ lib/core/components/app_bar/app_bar.dart
@@ -23,7 +23,7 @@ class AppBarComponent extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize {
-    return const Size(double.infinity, 50.0);
+    return const Size(double.infinity, 100.0);
   }
 
   @override
@@ -33,18 +33,35 @@ class AppBarComponent extends StatelessWidget implements PreferredSizeWidget {
         title,
         style: const TextStyle(
           fontWeight: FontWeight.bold,
-          color: Color.fromARGB(255, 63, 62, 62),
+          color: Colors.black,
           fontSize: 23,
         ),
       ),
       flexibleSpace: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 181, 179, 179),
-            Color.fromARGB(255, 110, 109, 109),
+            Color.fromARGB(255, 248, 249, 249),
+            Color.fromARGB(255, 53, 176, 242),
           ]),
         ),
       ),
+      bottom: const TabBar(tabs: <Widget>[
+        Text(
+          softWrap: false,
+          overflow: TextOverflow.visible,
+          '購入予定品',
+        ),
+        Text(
+          softWrap: false,
+          overflow: TextOverflow.visible,
+          '未評価',
+        ),
+        Text(
+          softWrap: false,
+          overflow: TextOverflow.visible,
+          '後悔度',
+        ),
+      ]),
     );
   }
 }
