9f43e65426538ed6c05e6f04373283184bec0729
Fudy-V <fudyV.test@gmail.com>
2024-11-09 17:10:40 +0900

    ⚡️ const位置の調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:kakubo/core/components/app_bar/app_bar.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:kakubo/core/components/app_bar/app_bar.dart';
 import 'package:kakubo/core/datasources/models/items.dart';
 import 'package:kakubo/screens/purchase_list.dart';
 import 'package:kakubo/screens/regret_list.dart';
 import 'package:kakubo/screens/unrated_list.dart';
-// import 'package:kakubo/screens/purchase_list.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:kakubo/features/input/entries/input.dart';
 
@@ -42,19 +40,19 @@ class MyApp extends StatelessWidget {
                   showModalBottomSheet(
                     context: context,
                     isScrollControlled: true,
-                    shape: RoundedRectangleBorder(
+                    shape: const RoundedRectangleBorder(
                       borderRadius:
                           BorderRadius.vertical(top: Radius.circular(20)),
                     ),
                     builder: (BuildContext context) {
-                      return InputItems();
+                      return const InputItems();
                     },
                   );
                 },
                 child: Icon(Icons.add),
               ),
               appBar: const AppBarComponent(title: 'KAKUBO'),
-              body: TabBarView(
+              body: const TabBarView(
                 children: <Widget>[
                   PurchaseList(),
                   UnratedList(),
