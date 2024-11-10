e970073b7021f632cca31c8c287e4798d8c82dd7
r-sei <ryousei3964@gmail.com>
2024-11-10 12:56:29 +0900

    ♻️ 未評価リストのサンプルのにようを変更

--- lib/screens/unrated_list.dart
+++ lib/screens/unrated_list.dart
@@ -11,12 +11,12 @@ class UnratedList extends StatefulWidget {
 class _UnratedListState extends State<UnratedList> {
   //サンプルのデータです
   final List<Map<String, dynamic>> list = [
-    {'date': '2024/11/09', 'itemName': 'pencil', 'price': '100'},
-    {'date': '2024/11/07', 'itemName': 'eraser', 'price': '120'},
-    {'date': '2024/11/07', 'itemName': 'macMiniPCM4chip', 'price': '200000'},
+    {'date': '2024/11/09', 'itemName': 'iphone15', 'price': '200000'},
+    {'date': '2024/11/07', 'itemName': 'umaibou', 'price': '110'},
+    {'date': '2024/11/07', 'itemName': 'pro controller', 'price': '5000'},
     {'date': '2024/11/07', 'itemName': 'pomodoroTimer', 'price': '1200'},
-    {'date': '2024/11/07', 'itemName': 'ChatGPTaccount', 'price': '1200'},
-    {'date': '2024/11/07', 'itemName': 'coffee', 'price': '540'}
+    {'date': '2024/11/07', 'itemName': 'gamble', 'price': '120000'},
+    {'date': '2024/11/07', 'itemName': 'tissue', 'price': '300'}
   ];
   @override
   Widget build(BuildContext context) {
